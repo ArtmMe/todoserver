b00e1f5156c1a121ae33a38aabe395560e86315e
ArtmMe <mr.remix_bite@mail.ru>
2025-07-24 14:34:34 +0300

    Базовый CORS

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('../routes/userRoutes');
 const todoRoutes = require('../routes/todoRoutes')
 const app = express();
@@ -7,7 +8,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 app.use('/user', userRoutes)
 
 const checkLogin = (req, res, next) => {
